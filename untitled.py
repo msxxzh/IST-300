87460d6d9ae9a450e909df521308ef9e1e2eb259
msxxzh <159365661+msxxzh@users.noreply.github.com>
2024-02-07 17:02:40 -0500

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 st.write('Hello World')
-st.balloons()
-st.checkbox('agree')
+if st.checkbox('agree') == True:
+  st.write ('Congratulations')
+  st.balloons()
