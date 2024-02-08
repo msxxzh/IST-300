ac9a5288f6746911dc3241b2b20bf053996e83f6
msxxzh <159365661+msxxzh@users.noreply.github.com>
2024-02-07 20:52:02 -0500

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,6 +1,6 @@
 import streamlit as st
-!pip install pip
-!pip install matplotlib
+pip install pip
+pip install matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 st.write('Hello World')
