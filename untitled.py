f1ec6b7cd6c0ee20fac5ee148c9e165ee33e24fd
msxxzh <159365661+msxxzh@users.noreply.github.com>
2024-02-07 20:49:59 -0500

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+python -m pip install -U pip
+python -m pip install -U matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 st.write('Hello World')
 if st.checkbox('agree') == True:
   st.write ('Congratulations')
