4e4f720b6ca871c1836da595988f2cbae20ca4eb
msxxzh <159365661+msxxzh@users.noreply.github.com>
2024-02-07 20:51:42 -0500

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,6 +1,6 @@
 import streamlit as st
-python -m pip install -U pip
-python -m pip install -U matplotlib
+!pip install pip
+!pip install matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 st.write('Hello World')
