29f6fdb403f85393a44cff4f91514cfc5d7fc293
msxxzh <159365661+msxxzh@users.noreply.github.com>
2024-02-19 15:57:41 -0500

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -11,5 +11,5 @@ st.button('Click')
 st.radio('Pick your gender',['Male','Female'])
 st.selectbox('Pick your gender',['Male','Female'])
 st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
-st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])s
-t.slider('Pick a number', 0,50)
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0,50)
