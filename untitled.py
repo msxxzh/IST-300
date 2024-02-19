97c06ee62285688fd6019c117ecea344e8a72fe7
msxxzh <159365661+msxxzh@users.noreply.github.com>
2024-02-19 16:27:12 -0500

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -26,7 +26,7 @@ df = pd.DataFrame(
     columns=['x','y','z'])
 
 c =alt.Chart(df).mark_circle().encode(   
-    x='x' , y='y' , size='z', color='z', tooltip ['x', 'y', 'z'])
+    x='x' , y='y' , size='z', color='z', tooltip= ['x', 'y', 'z'])
 
 st.altair_chart(c, use_container_width=True)
 
