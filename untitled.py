00eb2bc141a46bf1b65bc3b52e53d97d2b0735e3
msxxzh <159365661+msxxzh@users.noreply.github.com>
2024-02-19 16:24:39 -0500

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -21,3 +21,15 @@ st.color_picker('Choose your favorite color')
 st.progress(10)
 st.sidebar.title("Sidebar")
 
+df = pd.DataFrame(   
+    np.random.randn(500, 3),   
+    columns=['x','y','z'])
+
+c =alt.Chart(df).mark_circle().encode(   
+    x='x' , 'y'=y , size='z', color='z', tooltip ['x', 'y', 'z'])
+
+st.altair_chart(c, use_container_width=True)
+
+df = pd.DataFrame(np.random.randn(500, 2) / [50, 50] + [37.76, -122.4],
+columns=['lat', 'lon'])
+st.map(df)
